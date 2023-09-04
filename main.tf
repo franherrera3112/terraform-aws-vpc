acf9d81cb14dcf4338b6bd6b8f7b8c3f9fbfeae6
Francisco Herrera <franherrera3112@gmail.com>
2023-09-04 13:28:05 -0600

    Fixing error with name var

--- main.tf
+++ main.tf
@@ -137,8 +137,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = local.create_public_subnets && var.name != "vpn" ?  local.len_public_subnets : 0
-
+  count          = local.create_private_subnets && var.name != "vpn-${var.environment}-vpc"
   subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public[0].id
 }
@@ -269,8 +268,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count = local.create_private_subnets && var.name != "vpn" ? local.len_private_subnets : 0
+  # precondition {
+  #   condition = var.name == "vpn"
+  #   error_message = "VPN subnets cannot be associated with a route table"
+  # }
 
+  count = local.create_private_subnets && var.name != "vpn-${var.environment}-vpc" ? local.len_private_subnets : 0
+  
   subnet_id = element(aws_subnet.private[*].id, count.index)
   route_table_id = element(
     aws_route_table.private[*].id,
